636a1a772bc2f503c832117ac9529183dae356d5
ElsevetBo <hanwangyi95@163.com>
2021-02-21 15:17:04 +0800

    ADD union-find algo added

---
+++ algorithms/unionfind/UF.java
@@ -0,0 +1,49 @@
+public class UF {
+    private int count;
+    private int[] parent;
+    private int[] size;
+
+    public UF(int n) {
+        this.count = n;
+        this.parent = new int[n];
+        this.size = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            this.parent[i] = i;
+            this.size[i] = 1;
+        }
+    }
+
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+
+        if (rootP == rootQ) {
+            return;
+        }
+
+        if (size[rootP] > size[rootQ]) {
+            parent[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+        } else {
+            parent[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        }
+        count--;
+    }
+
+    public int find(int p) {
+        while (p != parent[p]) {
+            parent[p] = parent[parent[p]];
+            p = parent[p];
+        }
+
+        return p;
+    }
+
+    public boolean connected(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        return rootP == rootQ;
+    }
+}
